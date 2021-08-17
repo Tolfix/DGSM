e3eb158f0c4ee401e3a8a68f6d9bed6725c90504
Tolfix <matteus12122@gmail.com>
2021-08-17 14:55:23 +0200

    Modified MemeHandler (backend)

--- src/MemeHandler.ts
+++ src/MemeHandler.ts
@@ -1,6 +1,6 @@
 import { Router, Application } from "express";
 import io from "./Main";
-import { MemesId } from "./Memes/Meme";
+import { MemesId, MemeTemplate } from "./Memes/Meme";
 
 export default class MemeHandler
 {
@@ -13,7 +13,12 @@ export default class MemeHandler
             image: null,
             sound: "thud.mp3",
         },
-        "00002": null,
+        "00002": {
+            image: "sus.png",
+            name: "sussy_baka",
+            size: 128,
+            sound: "amugus.mp3",
+        },
     }
 
     constructor(server: Application) {
@@ -23,16 +28,37 @@ export default class MemeHandler
 
         this.router.post("/:memeId", async (req, res) => {
             const memeId = req.params.memeId as keyof MemesId
-
+            const body = req.body;
+            console.log(body);
             // Get our meme :D
-            io.emit("mem", this.getMeme(memeId));
-            return res.send(this.getMeme(memeId));
+            io.emit("mem", this.getMeme(memeId, body));
+            return res.send(this.getMeme(memeId, body));
         });
+
+        this.router.get("/memes", (req, res) => {
+            return res.send(this.Memes);
+        })
     }
 
-    public getMeme(memeId: keyof MemesId)
+    public getMeme(memeId: keyof MemesId, custom?: MemeTemplate)
     {
-        const meme = this.Memes[memeId];
+        let meme: any = this.Memes[memeId];
+
+        if(custom?.image)
+            meme.image = custom.image;
+
+        if(custom?.size)
+            meme.size = custom.size;
+
+        if(custom?.sound)
+            meme.sound = custom.sound;
+
+        if(custom?.x)
+            meme.x = custom.x;
+
+        if(custom?.y)
+            meme.y = custom.y;
+
         if(!meme)
             return { image: null, sound: null };
         return meme;

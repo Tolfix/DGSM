798b54108147dbb18d1fd9729984dac206a273e0
Tolfix <matteus12122@gmail.com>
2021-08-17 16:48:51 +0200

    Small changes

--- src/Main.ts
+++ src/Main.ts
@@ -19,7 +19,7 @@ server.use(cors({
 }));
 
 server.use(express.urlencoded({ extended: true }));
-server.use(express.json())
+server.use(express.json());
 
 server.use((req, res, next) => {
     res.locals.title = Title;

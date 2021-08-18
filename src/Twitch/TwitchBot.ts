c623cda125bacca55b230bca7c0015598f8ca186
Tolfix <matteus12122@gmail.com>
2021-08-18 13:13:48 +0200

    Dynamic replies

--- src/Twitch/TwitchBot.ts
+++ src/Twitch/TwitchBot.ts
@@ -20,23 +20,52 @@ export default class TwitchBot
     }
 
     private Memes: MemeHandler;
+    private Replies: Array<[string, string]> = [];
     
     constructor(memes: MemeHandler)
     {
         const client = tmijs.client(this.options);
         this.Memes = memes;
-        // this.Memes.cacheMemes();
+        this.cacheReplies();
         client.connect();
-        
+
         client.on('message', async (channel, userstate, message, self) => {
             message = message.toLocaleLowerCase();
             if(this.Memes.MemesName.get(message as keyof MemesId))
                 io.emit("mem", this.Memes.getMeme(message as keyof MemesId));
 
-            if(message.match(/gg/g))
-                io.emit("mem", this.Memes.getMeme("" as keyof MemesId, {
-                    text: `${userstate.username} fuck you :D`,
-                }))
+            for(const reply of this.Replies)
+            {
+                if(message.match(new RegExp(reply[0], "g")))
+                    io.emit("mem", this.Memes.getMeme("" as keyof MemesId, {
+                        text: this.formatReply(reply[0], {channel, userstate, message, self}),
+                    }));
+            }
         });
+    }
+
+    public formatReply(text: string, data: { channel: string, userstate: tmijs.ChatUserstate, message: string, self: boolean })
+    {
+        // Replace if user
+        text = text.replace(/{user}/g, data.userstate.username ?? "");
+
+        return text;
+    }
+
+    public cacheReplies()
+    {
+        let count = 1;
+        while(true)
+        {
+            let reply = process.env[`TWITCH_REPLY_${count}`];
+            if(!reply)
+                break;
+
+            const list = JSON.parse(reply);
+            console.log(list)
+            this.Replies.push(list);
+
+            count++;
+        }
     }
 }

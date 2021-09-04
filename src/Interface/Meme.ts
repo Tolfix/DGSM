90b0bcf5e9f09bcd6bf41608f5ab580bfdef4440
Matteus <tolfx@tolfix.com>
2021-09-04 13:55:43 +0200

    Removed < MemesId >

--- src/Interface/Meme.ts
+++ src/Interface/Meme.ts
@@ -1,12 +1,3 @@
-import ThudMeme from "../Memes/General/Thud";
-import SusMeme from "../Memes/Sus/Sus";
-
-export interface MemesId
-{
-    "00001": typeof ThudMeme;
-    "00002": typeof SusMeme;
-}
-
 export interface MemeTemplate
 {
     id: string;

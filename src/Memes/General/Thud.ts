f4922654ee351304957fc8ee164fb0574be96f15
Matteus <tolfx@tolfix.com>
2021-09-04 13:55:21 +0200

    removed memes

--- src/Memes/General/Thud.ts
+++
@@ -1,9 +0,0 @@
-import { MemeTemplate } from "../../Interface/Meme"
-
-const ThudMeme: Partial<MemeTemplate> = {
-    id: "00001",
-    name: "thud",
-    sound: "thud.mp3",
-}
-
-export default ThudMeme
